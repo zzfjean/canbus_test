0ea56781b69eeac335da9201aaff08bd568291af
shutong <285547719@qq.com>
2021-11-12 16:20:03 -0700

    add valid message identifier

--- broadcast_stop.py
+++ broadcast_stop.py
@@ -6,13 +6,14 @@ import random
 random.seed(100)
 
 
-def send_can_message(credential):
+def send_can_message(credential, message_identifier):
     can_prefix = 'cansend can0 '
     # CxD -> machine command
     PGN = '04F21100'
     # emergency stop command
     SPN_command = '0B0000'
-    can_message = can_prefix + PGN + '#' + SPN_command + credential + '00'
+    message_identifier = '{:02x}'.format(message_identifier)
+    can_message = can_prefix + PGN + '#' + SPN_command + credential + message_identifier
     no_canbus = os.system(can_message)
     if no_canbus:
         return False    # there is no canbus available
@@ -26,6 +27,7 @@ while True:
     if can_available:
         start = time.time()
         credential = '{:08x}'.format(random.randrange(2**32 - 1))
-        can_available = send_can_message(credential)
+        can_available = send_can_message(credential, count)
+        count = count + 1 if count < 255 else 0
         print(time.time() - start)
     time.sleep(0.5)
