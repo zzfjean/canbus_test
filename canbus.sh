e83f0371b64d317a4f4f53c2582a85cc8452ed7e
shutong <285547719@qq.com>
2021-11-15 14:59:12 -0700

    add the bash file, which is used to enable canbus on tx2 to github

---
+++ canbus.sh
@@ -0,0 +1,7 @@
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe mttcan
+sudo ip link set can0 type can bitrate 500000
+sudo ip link set up can0
+sudo ip link set can1 type can bitrate 500000
+sudo ip link set up can1
